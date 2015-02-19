a6453d25cdb37226fb557daa6c07e11a222b5fe6
iqupencil <kare@lds.net.ua>
2015-02-19 15:12:17 +0300

    I do it

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once "../vendor/autoload.php";
 require('../vendor/autoload.php');
 
 $app = new Silex\Application();
@@ -19,4 +19,5 @@ $app->get('/', function() use($app) {
 
 $app->run();
 
+echo "Hello World";
 ?>
